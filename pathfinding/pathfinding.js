70c354880c00bc0ccdfec535c57051bd1d03eac1
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-02-18 15:26:45 +0200

    feat: add .npmignore files and initial type definitions for various modules

---
+++ pathfinding/pathfinding.js
@@ -0,0 +1,171 @@
+const logMaze = require("./logger");
+const NO_ONE = 0;
+const BY_A = 1;
+const BY_B = 2;
+
+// write in a function thats a X by X array of arrays of numbers
+// as well two x/y combinations and have it return the shortest
+// length (you don't need to track the actual path) from point A
+// to point B.
+//
+// the numbers in the maze array represent as follows:
+// 0 – open space
+// 1 - closed space, cannot pass through. a wall
+// 2 - one of the two origination points
+//
+// you will almost certainly need to transform the maze into your own
+// data structure to keep track of all the meta data
+const findShortestPathLength = (maze, [xA, yA], [xB, yB]) => {
+  const visited = maze.map((row, y) =>
+    row.map((origin, x) => ({
+      closed: origin === 1,
+      length: 0,
+      openedBy: NO_ONE,
+      x,
+      y
+    }))
+  );
+  visited[yA][xA].openedBy = BY_A;
+  visited[yB][xB].openedBy = BY_B;
+  logMaze(visited);
+
+  let aQueue = [visited[yA][xA]];
+  let bQueue = [visited[yB][xB]];
+  let iteration = 0;
+
+  // if one runs out, there's no path
+  while (aQueue.length && bQueue.length) {
+    iteration++;
+    const aNeighbors = aQueue.reduce(
+      (acc, neighbor) =>
+        acc.concat(getNeighbors(visited, neighbor.x, neighbor.y)),
+      []
+    );
+    aQueue = [];
+    for (let i = 0; i < aNeighbors.length; i++) {
+      const neighbor = aNeighbors[i];
+      if (neighbor.openedBy === BY_B) {
+        return neighbor.length + iteration;
+      } else if (neighbor.openedBy === NO_ONE) {
+        neighbor.length = iteration;
+        neighbor.openedBy = BY_A;
+        aQueue.push(neighbor);
+      }
+    }
+
+    const bNeighbors = bQueue.reduce(
+      (acc, neighbor) =>
+        acc.concat(getNeighbors(visited, neighbor.x, neighbor.y)),
+      []
+    );
+    bQueue = [];
+    for (let i = 0; i < bNeighbors.length; i++) {
+      const neighbor = bNeighbors[i];
+      if (neighbor.openedBy === BY_A) {
+        return neighbor.length + iteration;
+      } else if (neighbor.openedBy === NO_ONE) {
+        neighbor.length = iteration;
+        neighbor.openedBy = BY_B;
+        bQueue.push(neighbor);
+      }
+    }
+    logMaze(visited);
+  }
+  return -1;
+};
+
+const getNeighbors = (visited, x, y) => {
+  const neighbors = [];
+
+  if (y - 1 >= 0 && !visited[y - 1][x].closed) {
+    // left
+    neighbors.push(visited[y - 1][x]);
+  }
+
+  if (y + 1 < visited[0].length && !visited[y + 1][x].closed) {
+    // right
+    neighbors.push(visited[y + 1][x]);
+  }
+
+  if (x - 1 >= 0 && !visited[y][x - 1].closed) {
+    // up
+    neighbors.push(visited[y][x - 1]);
+  }
+
+  if (x + 1 < visited.length && !visited[y][x + 1].closed) {
+    // down
+    neighbors.push(visited[y][x + 1]);
+  }
+
+  return neighbors;
+};
+
+// unit tests
+// do not modify the below code
+  const fourByFour = [
+    [2, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 2]
+  ];
+  findShortestPathLength(fourByFour, [0, 0], [3, 3]);
+
+  const sixBySix = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 0, 0, 0]
+  ];
+  findShortestPathLength(sixBySix, [1, 1], [2, 5]);
+
+  const eightByEight = [
+    [0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 2, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 2]
+  ];
+  findShortestPathLength(eightByEight, [1, 7], [7, 7]);
+
+  const fifteenByFifteen = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0, 1, 1, 2, 1, 0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  findShortestPathLength(fifteenByFifteen, [1, 1], [8, 8]);
+
+  const byEachOther = [
+    [0, 0, 0, 0, 0],
+    [0, 2, 2, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0]
+  ];
+  findShortestPathLength(byEachOther, [1, 1], [2, 1]);
+  const impossible = [
+    [0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0],
+    [0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 2]
+  ];
+ findShortestPathLength(impossible, [1, 1], [4, 4]);
+
+  console.log(";;"); // why when i run the program it doesn't print the console.log(";;")?
